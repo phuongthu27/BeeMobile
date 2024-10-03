a496674dd6fa055bf625a0efa45e3c3a7be37d26
PhamTheHien3333 <phamthehienapple@gmail.com>
2024-10-03 08:50:09 +0700

    tabBottom

--- Bee_Mobile/App.js
+++ Bee_Mobile/App.js
@@ -1,12 +1,105 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+
+
+function HomeScreen() {
+    return (
+        <View style={{flex: 1, alignItem: 'center', justifyContent: 'center'}}>
+            <Text>
+                Home Screen
+            </Text>
+        </View>
+    );
+}
+
+function TransactionScreen() {
+  return (
+    <View >
+        <Text>
+            Home Screen
+        </Text>
+    </View>
+    );
+}
+
+function AddScreen() {
+  return (
+    <View >
+        <Text>
+            Home Screen
+        </Text>
+    </View>
+    );
+}
+
+function BudgetScreen() {
+  return (
+    <View >
+        <Text>
+            Home Screen
+        </Text>
+    </View>
+    );
+}
+
+function ProfileScreen() {
+  return (
+    <View >
+        <Text>
+            Home Screen
+        </Text>
+    </View>
+    );
+}
+
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+          screenOptions = {({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+                let IconComponent;
+    
+                if (route.name === "Tổng quan") {
+                  iconName = "home";
+                  IconComponent = FontAwesomeIcon; // Sử dụng FontAwesome cho "Tổng quan"
+                } else if (route.name === "Giao dịch") {
+                  iconName = "account-balance-wallet";
+                  IconComponent = MaterialIcon; // Sử dụng MaterialIcons cho "Giao dịch"
+                } else if (route.name === "Add") {
+                  iconName = "add-circle";
+                  IconComponent = MaterialIcon;
+                } else if (route.name === "Ngân sách") {
+                  iconName = "bank";
+                  IconComponent = FontAwesomeIcon;
+                } else if (route.name === "Tài khoản") {
+                  iconName = "user";
+                  IconComponent = FontAwesomeIcon;
+                }
+                return <IconComponent name={iconName} size={size} color={focused ? color : 'black'} />;
+              },
+          })}
+      >
+        <Tab.Screen name="Tổng quan" component={HomeScreen} />
+        <Tab.Screen name="Giao dịch" component={TransactionScreen} />
+        <Tab.Screen name="Add" component={AddScreen} options={{tabBarLabel: () => null, tabBarIcon: ({ focused }) => (
+          <MaterialIcon 
+            name="add-circle" 
+            size={focused ? 50 : 45} 
+            color="#5D0DE1" 
+          />
+        ),}}/>
+        <Tab.Screen name="Ngân sách" component={BudgetScreen} />
+        <Tab.Screen name="Tài khoản" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
