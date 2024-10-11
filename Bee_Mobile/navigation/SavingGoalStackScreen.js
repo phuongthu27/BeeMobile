8d80ebd6c9ebd411277df74db9c9f611206356d3
Bao2912 <thienbaoct12@gmail.com>
2024-10-11 09:25:43 +0700

    chuyển screen

---
+++ Bee_Mobile/navigation/SavingGoalStackScreen.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Text, View, TextInput, Image, TouchableOpacity } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import SavingGoalScreen from '../screens/SavingGoalScreen/SavingGoalList';
+import SavingGoalAdd from '../screens/SavingGoalScreen/SavingGoalAdd';
+import SavingGoalEdit from '../screens/SavingGoalScreen/SavingGoalEdit';
+import SavingGoalDetail from '../screens/SavingGoalScreen/SavingGoalDetail';
+
+const SavingGoalStack = createStackNavigator();
+
+
+    function SavingGoalStackScreen() {
+        return (
+          <SavingGoalStack.Navigator>
+            <SavingGoalStack.Screen 
+              name="SavingGoalList" 
+              component={SavingGoalScreen} 
+              options={{ title: 'Danh sách mục tiêu' }} 
+            />
+            <SavingGoalStack.Screen 
+              name="SavingGoalAdd" 
+              component={SavingGoalAdd} 
+              options={{ title: 'Thêm mục tiêu' }} 
+            />
+            <SavingGoalStack.Screen 
+              name="SavingGoalEdit" 
+              component={SavingGoalEdit} 
+              options={{ title: 'Sửa mục tiêu' }} 
+            />
+            <SavingGoalStack.Screen 
+              name="SavingGoalDetail" 
+              component={SavingGoalDetail} 
+              options={{ title: 'Chi tiết mục tiêu' }} 
+            />
+          </SavingGoalStack.Navigator>
+        );
+      };
+
+export default SavingGoalStackScreen;
---
+++ Bee_Mobile/navigation/TabNavigator.js
@@ -0,0 +1,71 @@
+import React from "react";
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import HomeScreen from '../screens/HomeScreen';
+import BudgetScreen from '../screens/BudgetScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import TransactionScreen from '../screens/TransactionScreen';
+import SavingGoalStackScreen from './SavingGoalStackScreen';
+
+const Tab = createBottomTabNavigator();
+
+function TabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => {
+        let iconName;
+        let IconComponent;
+
+        if (route.name === "Tổng quan") {
+          iconName = "home";
+          IconComponent = FontAwesomeIcon;
+        } else if (route.name === "Mục tiêu") {
+          iconName = "account-balance-wallet";
+          IconComponent = MaterialIcon;
+        } else if (route.name === "Giao dịch") {
+          iconName = "add-circle";
+          IconComponent = MaterialIcon;
+        } else if (route.name === "Ngân sách") {
+          iconName = "bank";
+          IconComponent = FontAwesomeIcon;
+        } else if (route.name === "Tài khoản") {
+          iconName = "user";
+          IconComponent = FontAwesomeIcon;
+        }
+
+        return {
+          tabBarIcon: ({ focused, color, size }) => (
+            <IconComponent
+              name={iconName}
+              size={size}
+              color={focused ? color : "black"}
+            />
+          ),
+          headerShown: false,
+        };
+      }}
+    >
+      <Tab.Screen name="Tổng quan" component={HomeScreen} />
+      <Tab.Screen name="Mục tiêu" component={SavingGoalStackScreen} />
+      <Tab.Screen
+        name="Giao dịch"
+        component={TransactionScreen}
+        options={{
+          tabBarLabel: () => null,
+          tabBarIcon: ({ focused }) => (
+            <MaterialIcon
+              name="add-circle"
+              size={focused ? 50 : 45}
+              color="#5D0DE1"
+            />
+          ),
+        }}
+      />
+      <Tab.Screen name="Ngân sách" component={BudgetScreen} />
+      <Tab.Screen name="Tài khoản" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default TabNavigator;
